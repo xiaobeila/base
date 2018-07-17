f3de52d1c76a9c6f0cea6bb6abf3c19cd57a2d86
王晓北 <xiaobei.wang@fq960.com>
2018-07-17 22:10:51 +0800

    【base】修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,43 +4,28 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
+        <a href="https://vuejs.org" target="_blank">
           Core Docs
         </a>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
+        <a href="https://forum.vuejs.org" target="_blank">
           Forum
         </a>
       </li>
       <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
+        <a href="https://chat.vuejs.org" target="_blank">
           Community Chat
         </a>
       </li>
       <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
+        <a href="https://twitter.com/vuejs" target="_blank">
           Twitter
         </a>
       </li>
       <br>
       <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
           Docs for This Template
         </a>
       </li>
@@ -48,55 +33,65 @@
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://router.vuejs.org/" target="_blank">
           vue-router
         </a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vuex.vuejs.org/" target="_blank">
           vuex
         </a>
       </li>
       <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vue-loader.vuejs.org/" target="_blank">
           vue-loader
         </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
           awesome-vue
         </a>
       </li>
     </ul>
+    <div class="components-container" style='height:100vh'>
+      <div class='chart-container'>
+        <keyboard-chart v-if="chartData && chartKeyData" height='100%' width='100%' :chartData="chartData" :chartKeyData="chartKeyData" :name="charName"></keyboard-chart>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import keyboardChart from 'base/Charts/keyboard'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      chartData: null,
+      chartKeyData: null,
+      charName: '小林'
     }
+  },
+  components: { keyboardChart },
+  mounted () {
+    let xAxisData = []
+    let data = []
+    for (let i = 0; i < 30; i++) {
+      xAxisData.push(i + '号')
+      data.push(Math.round(Math.random() * 2 + 3))
+    }
+    this.chartData = xAxisData
+    this.chartKeyData = data
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -109,5 +104,10 @@ li {
 }
 a {
   color: #42b983;
+}
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: 90%;
 }
 </style>
